e86b4955c8ffddbdf7a1218ba89a458ce9c7f614
Lucas <lucasdgonzalez@outlook.com>
2023-07-22 00:10:11 -0300

    Agrega funcionalidad a boton siguiente pagina
    Se agregó la funcionalidad del botón siguiente página. Al darle click se hace la solicitud correspondiente para mostrar los siguientes 20 pokémon, mostrar su respectiva información, y activar el botón anterior página.
    
    También se modificaron las funciones que se ocupan de mostrar la información de los pokémon, la función para gestionar la API, y el request para que se adapten a la funcionalidad de los botones para cambiar página.

--- docs/index.js
+++ docs/index.js
@@ -1,61 +1,73 @@
+const $botonSiguientePagina = document.querySelector(".boton-siguiente-pagina");
+const $botonAnteriorPagina = document.querySelector(".boton-anterior-pagina");
+
+let indicadorPágina = 1;
+
 async function hacerSolicitud() {
-  try {
-    const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const data = await respuesta.json();
-    // console.log(data.results); // Imprime la información general de los primeros 20 pokémon
-    gestionarAPI(data);
-  } catch (error) {}
+  const cantidadPokemonPorPagina = 20;
+  let indicadorPokemon = indicadorPágina * cantidadPokemonPorPagina;
+  
+  if (indicadorPágina === 1) {
+    try {
+      const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await respuesta.json();
+      console.log(data); // Imprime la información general de los primeros 20 pokémon
+      gestionarAPI(data);
+    } catch (error) {}
+  } else {
+    try {
+      const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${indicadorPokemon}&limit=20`);
+      const data = await respuesta.json();
+      console.log(data); // Imprime la información general de los primeros 20 pokémon
+      gestionarAPI(data);
+    } catch (error) {}
+  }
 }
 hacerSolicitud();
 
 function gestionarAPI(data) {
   for (let i = 0; i < data.results.length; i++) {
     let urlPokemonABuscar = data.results[i]["url"];
-    gestionarInformacionPokemon(urlPokemonABuscar);
+    let indicadorPosicionPokemonEnLista = i;
+    gestionarInformacionPokemon(urlPokemonABuscar, indicadorPosicionPokemonEnLista);
   }
 }
 
-async function gestionarInformacionPokemon(URL) {
+async function gestionarInformacionPokemon(URL, indicadorPosicionPokemonEnLista) {
   try {
     const respuesta = await fetch(URL);
     const data = await respuesta.json();
     // console.log(data); // Imprime información del pokémon correspondiente
-    mostrarImagenPokemon(data);
-    mostrarNombrePokemon(data);
-    mostrarIdentificacionPokemon(data);
-    mostrarTiposPokemon(data);
+    mostrarImagenPokemon(data, indicadorPosicionPokemonEnLista);
+    mostrarNombrePokemon(data, indicadorPosicionPokemonEnLista);
+    mostrarIdentificacionPokemon(data, indicadorPosicionPokemonEnLista);
+    mostrarTiposPokemon(data, indicadorPosicionPokemonEnLista);
   } catch (error) {
     console.error(error);
   }
 }
 
-function mostrarImagenPokemon(dataPokemon) {
-  let indicadorCartaPokemon = dataPokemon.id - 1;
-
+function mostrarImagenPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
   const $imagenesCartasPokemon = document.querySelectorAll(".imagen-carta");
-  $imagenesCartasPokemon[indicadorCartaPokemon].src = dataPokemon.sprites["front_default"];
-  $imagenesCartasPokemon[indicadorCartaPokemon].alt = `Pokémon ${dataPokemon.name}`;
+  $imagenesCartasPokemon[indicadorPosicionPokemonEnLista].src = dataPokemon.sprites["front_default"];
+  $imagenesCartasPokemon[indicadorPosicionPokemonEnLista].alt = `Pokémon ${dataPokemon.name}`;
 }
 
-function mostrarNombrePokemon(dataPokemon) {
-  let indicadorCartaPokemon = dataPokemon.id - 1;
-
+function mostrarNombrePokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
   const $titulosCartasPokemon = document.querySelectorAll(".nombre-pokemon");
-  $titulosCartasPokemon[indicadorCartaPokemon].textContent = dataPokemon.name;
+  $titulosCartasPokemon[indicadorPosicionPokemonEnLista].textContent = dataPokemon.name;
 }
 
-function mostrarIdentificacionPokemon(dataPokemon) {
-  let indicadorCartaPokemon = dataPokemon.id - 1;
-
+function mostrarIdentificacionPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
   const $numeroIdentificacionPokemon = document.querySelectorAll(".numero-identificacion-pokemon");
-  $numeroIdentificacionPokemon[indicadorCartaPokemon].textContent = `# ${dataPokemon.id}`;
+  $numeroIdentificacionPokemon[indicadorPosicionPokemonEnLista].textContent = `# ${dataPokemon.id}`;
 }
 
-function mostrarTiposPokemon(dataPokemon) {
-  let indicadorCartaPokemon = dataPokemon.id - 1;
+function mostrarTiposPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
   const cantidadTiposPokemon = dataPokemon.types.length;
 
-  const $contenedorTiposPokemon = document.querySelectorAll(".contenedor-tipos-pokemon")[indicadorCartaPokemon];
+  const $contenedorTiposPokemon =
+    document.querySelectorAll(".contenedor-tipos-pokemon")[indicadorPosicionPokemonEnLista];
 
   if (cantidadTiposPokemon === 1) {
     const $primerImagenTipoPokemon = $contenedorTiposPokemon.children[0].children[0];
@@ -71,3 +83,19 @@ function mostrarTiposPokemon(dataPokemon) {
     $segundaImagenTipoPokemon.src = `../img/${dataPokemon.types["1"]["type"]["name"]}.svg`;
   }
 }
+
+function activarBoton(){
+  const $botonesCambiarPagina = document.querySelectorAll (".boton-cambiar-pagina");
+
+  $botonesCambiarPagina.forEach((boton) => {
+    if(boton.id !== ""){
+      boton.id = "";
+    }
+  })
+}
+
+$botonSiguientePagina.addEventListener("click", () => {
+  indicadorPágina++;
+  hacerSolicitud();
+  activarBoton();
+});
