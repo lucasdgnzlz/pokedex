25bdbb6050c35574f537d67fc5cc9f5f425bd565
Lucas <lucasdgonzalez@outlook.com>
2023-07-25 23:00:33 -0300

    Crea carta de pokémon detallada
    Se agregó stats a los pokemon

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,16 @@
-const $botonBuscarPokemonPorId = document.querySelector(".boton-buscar-pokemon");
+const $botonBuscarPokemon = document.querySelector(".boton-buscar-pokemon");
+const $botonCerrarDetallesPokemon = document.querySelector(".boton-cerrar-detalles");
 
 const $botonSiguientePagina = document.querySelector(".boton-siguiente-pagina");
 const $botonAnteriorPagina = document.querySelector(".boton-anterior-pagina");
 
-let indicadorPagina = 1;
+let indicadorPagina = 0;
 
 async function hacerSolicitud() {
   const cantidadPokemonPorPagina = 20;
   let indicadorPokemon = indicadorPagina * cantidadPokemonPorPagina;
 
-  if (indicadorPagina === 1) {
+  if (indicadorPagina === 0) {
     try {
       const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon");
       const data = await respuesta.json();
@@ -87,6 +88,110 @@ function mostrarTiposPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
   }
 }
 
+async function buscarPokemonPorId(idPokemon) {
+  try {
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+    const data = await respuesta.json();
+    console.log(data);
+    mostrarCartaPokemonElegido();
+    mostrarImagenPokemonElegido(data);
+    mostrarIdPokemonElegido(data);
+    mostrarNombrePokemonElegido(data);
+    mostrarTiposPokemonElegido(data);
+    mostrarStatsPokemon(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function mostrarImagenPokemonElegido(dataPokemon) {
+  const $imagenPokemonElegido = document.querySelector(".imagen-pokemon-elegido");
+  $imagenPokemonElegido.src = dataPokemon.sprites["front_default"];
+  $imagenPokemonElegido.alt = `Pokémon ${dataPokemon.name}`;
+}
+
+function mostrarIdPokemonElegido(dataPokemon) {
+  const $idPokemonElegido = document.querySelector(".id-pokemon-elegido");
+  $idPokemonElegido.textContent = `# ${dataPokemon.id}`;
+}
+
+function mostrarNombrePokemonElegido(dataPokemon) {
+  const $nombrePokemonElegido = document.querySelector(".nombre-pokemon-elegido");
+  $nombrePokemonElegido.textContent = dataPokemon.name;
+}
+
+function mostrarTiposPokemonElegido(dataPokemon) {
+  const cantidadTiposPokemon = dataPokemon.types.length;
+
+  if (cantidadTiposPokemon === 1) {
+    const $primerImagenTipoPokemon = document.querySelector(".primer-tipo-pokemon-elegido");
+    $primerImagenTipoPokemon.src = `../img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
+
+    const $segundaImagenTipoPokemon = document.querySelector(".segundo-tipo-pokemon-elegido");
+    $segundaImagenTipoPokemon.id = "oculto";
+  } else {
+    const $primerImagenTipoPokemon = document.querySelector(".primer-tipo-pokemon-elegido");
+    const $segundaImagenTipoPokemon = document.querySelector(".segundo-tipo-pokemon-elegido");
+
+    $primerImagenTipoPokemon.src = `../img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
+    $segundaImagenTipoPokemon.src = `../img/${dataPokemon.types["1"]["type"]["name"]}.svg`;
+    $segundaImagenTipoPokemon.id = "";
+  }
+}
+
+function mostrarStatsPokemon(dataPokemon) {
+  let vidaPokemon = dataPokemon.stats["0"]["base_stat"];
+  let ataquePokemon = dataPokemon.stats["1"]["base_stat"];
+  let ataqueEspecialPokemon = dataPokemon.stats["2"]["base_stat"];
+  let defensaPokemon = dataPokemon.stats["3"]["base_stat"];
+  let defensaEspecialPokemon = dataPokemon.stats["4"]["base_stat"];
+  let velocidadPokemon = dataPokemon.stats["5"]["base_stat"];
+
+  const $respuestaVidaPokemon = document.querySelector(".vida-base-respuesta");
+  const $respuestaAtaquePokemon = document.querySelector(".ataque-base-respuesta");
+  const $respuestaDefensaPokemon = document.querySelector(".defensa-base-respuesta");
+  const $respuestaAtaqueEspecialPokemon = document.querySelector(".ataque-especial-base-respuesta");
+  const $respuestaDefensaEspecialPokemon = document.querySelector(".defensa-especial-base-respuesta");
+  const $respuestaVelocidadPokemon = document.querySelector(".velocidad-base-respuesta");
+
+  $respuestaVidaPokemon.textContent = vidaPokemon;
+  $respuestaAtaquePokemon.textContent = ataquePokemon;
+  $respuestaDefensaPokemon.textContent = defensaPokemon;
+  $respuestaAtaqueEspecialPokemon.textContent = ataqueEspecialPokemon;
+  $respuestaDefensaEspecialPokemon.textContent = defensaEspecialPokemon;
+  $respuestaVelocidadPokemon.textContent = velocidadPokemon;
+}
+
+function esconderGrilla() {
+  const $grillaPokemon = document.querySelector(".contenedor-grilla");
+  $grillaPokemon.id = "oculto";
+}
+
+function mostrarGrilla() {
+  const $grillaPokemon = document.querySelector(".contenedor-grilla");
+  $grillaPokemon.id = "";
+}
+
+function esconderCambioPagina() {
+  const $contenedorCambioPagina = document.querySelector(".contenedor-cambio-pagina");
+  $contenedorCambioPagina.id = "oculto";
+}
+
+function mostrarCambioPagina() {
+  const $contenedorCambioPagina = document.querySelector(".contenedor-cambio-pagina");
+  $contenedorCambioPagina.id = "";
+}
+
+function mostrarCartaPokemonElegido() {
+  const $cartaPokemonElegido = document.querySelector(".carta-respuesta");
+  $cartaPokemonElegido.id = "";
+}
+
+function ocultarCartaPokemonElegido() {
+  const $cartaPokemonElegido = document.querySelector(".carta-respuesta");
+  $cartaPokemonElegido.id = "oculto";
+}
+
 function mostrarErrorValidacionBuscador(error) {
   const $buscadorPokemon = document.querySelector(".buscador-pokemon");
   $buscadorPokemon.classList.add("is-invalid");
@@ -101,7 +206,7 @@ function eliminarErrorValidacion() {
   $buscadorPokemon.id = "";
 }
 
-$botonBuscarPokemonPorId.addEventListener("click", () => {
+$botonBuscarPokemon.addEventListener("click", () => {
   const idPokemonABuscar = document.querySelector(".buscador-pokemon").value;
 
   let error = validarIdPokemon(idPokemonABuscar);
@@ -109,9 +214,17 @@ $botonBuscarPokemonPorId.addEventListener("click", () => {
   if (error !== "") {
     mostrarErrorValidacionBuscador(error);
   } else {
-    console.log("ta bien");
+    buscarPokemonPorId(idPokemonABuscar);
+    esconderGrilla();
+    esconderCambioPagina();
     eliminarErrorValidacion();
   }
+});
+
+$botonCerrarDetallesPokemon.addEventListener("click", () => {
+  ocultarCartaPokemonElegido();
+  mostrarGrilla();
+  mostrarCambioPagina();
 });
 
 function activarBoton() {
@@ -143,7 +256,7 @@ $botonSiguientePagina.addEventListener("click", () => {
 });
 
 $botonAnteriorPagina.addEventListener("click", () => {
-  if (indicadorPagina === 2) {
+  if (indicadorPagina === 1) {
     indicadorPagina--;
     hacerSolicitud();
     activarBoton();
