1c3290181802b8e7fa4cf6d808ba9578b7eb06e1
Lucas <lucasdgonzalez@outlook.com>
2023-09-19 15:35:48 -0300

    Agrega pokedex.js
    Se agregó para separar las funciones encargadas de hacer el request a la API.

---
+++ docs/pokedex.js
@@ -0,0 +1,29 @@
+export async function hacerSolicitud(indicadorPokemon) {
+  try {
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${indicadorPokemon}&limit=20`);
+    const data = await respuesta.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function buscarPokemonEspecifico(URL) {
+  try {
+    const respuesta = await fetch(URL);
+    const data = respuesta.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function buscarPokemonPorId(idPokemon) {
+  try {
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+    const data = await respuesta.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
