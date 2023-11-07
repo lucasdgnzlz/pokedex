2eb324b45edf179777734d5e275abf7373b00f79
Lucas <lucasdgonzalez@outlook.com>
2023-11-07 17:41:16 -0300

    Cambia de nombre el archivo
    ui.js pasa a ser general.js

--- src/ui/general.js
+++ src/ui/general.js
@@ -1,8 +1,8 @@
 import { hacerSolicitud, buscarPokemonEspecifico } from "../api/pokedex.js";
 import { validarIdPokemon } from "../validaciones/validaciones.js";
 import { cargarPokemonesDeLocalStorage, guardarPokemonesEnLocalStorage, cargarDataPokemonDeLocalStorage, guardarDataPokemonEnLocalStorage } from "../storage/pokedex.js";
-import {mostrarErrorValidacionBuscador, eliminarErrorValidacion} from "./validacionBuscador.js";
-
+import { mostrarErrorValidacionBuscador, eliminarErrorValidacion} from "./validacionBuscador.js";
+import { mostrarImagenPokemon, mostrarNombrePokemon, mostrarIdentificacionPokemon, mostrarTiposPokemon, mostrarImagenPokemonElegido, mostrarIdPokemonElegido, mostrarNombrePokemonElegido, mostrarTiposPokemonElegido, mostrarStatsPokemon} from "./cartasPokemon.js";
 
 export async function gestionarPaginas() {
 	const numeroPaginaActual = Number(document.querySelector(".active").textContent);
@@ -203,115 +203,6 @@ export function gestionarCambioPaginaSiguiente($indicadoresPagina) {
 		mostrarNumeroPaginaActiva(numeroPaginaSolicitada);
 		gestionarPaginas();
 	}
-}
-
-function mostrarImagenPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
-	if(typeof dataPokemon.sprites === "string"){
-		const $imagenesCartasPokemon = document.querySelectorAll(".imagen-carta");
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].src = dataPokemon.sprites;
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].alt = `Pokémon ${dataPokemon.name}`;
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].id = `${dataPokemon.name}`;
-	} else{
-		const $imagenesCartasPokemon = document.querySelectorAll(".imagen-carta");
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].src = dataPokemon.sprites["front_default"];
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].alt = `Pokémon ${dataPokemon.name}`;
-		$imagenesCartasPokemon[indicadorPosicionPokemonEnLista].id = `${dataPokemon.name}`;
-	}
-}
-
-function mostrarNombrePokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
-	const $titulosCartasPokemon = document.querySelectorAll(".nombre-pokemon");
-	$titulosCartasPokemon[indicadorPosicionPokemonEnLista].textContent = dataPokemon.name;
-}
-
-function mostrarIdentificacionPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
-	const $numeroIdentificacionPokemon = document.querySelectorAll(".numero-identificacion-pokemon");
-	$numeroIdentificacionPokemon[indicadorPosicionPokemonEnLista].textContent = `# ${dataPokemon.id}`;
-}
-
-function mostrarTiposPokemon(dataPokemon, indicadorPosicionPokemonEnLista) {
-	const cantidadTiposPokemon = dataPokemon.types.length;
-
-	const $contenedorTiposPokemon = document.querySelectorAll(".contenedor-tipos-pokemon")[indicadorPosicionPokemonEnLista];
-
-	if (cantidadTiposPokemon === 1) {
-		const $primerImagenTipoPokemon = $contenedorTiposPokemon.children[0].children[0];
-		$primerImagenTipoPokemon.src = `img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
-
-		const $segundaImagenTipoPokemon = $contenedorTiposPokemon.children[1].children[0];
-		$segundaImagenTipoPokemon.id = "oculto";
-	} else {
-		const $primerImagenTipoPokemon = $contenedorTiposPokemon.children[0].children[0];
-		const $segundaImagenTipoPokemon = $contenedorTiposPokemon.children[1].children[0];
-
-		$primerImagenTipoPokemon.src = `img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
-		$segundaImagenTipoPokemon.src = `img/${dataPokemon.types["1"]["type"]["name"]}.svg`;
-		$segundaImagenTipoPokemon.id = "";
-	}
-}
-
-function mostrarImagenPokemonElegido(dataPokemon) {
-	const $imagenPokemonElegido = document.querySelector(".imagen-pokemon-elegido");
-
-	if(typeof dataPokemon.sprites === "string"){
-		$imagenPokemonElegido.src = dataPokemon.sprites;
-		$imagenPokemonElegido.alt = `Pokémon ${dataPokemon.name}`;
-	} else{
-		$imagenPokemonElegido.src = dataPokemon.sprites["front_default"];
-		$imagenPokemonElegido.alt = `Pokémon ${dataPokemon.name}`;
-	}
-}
-
-function mostrarIdPokemonElegido(dataPokemon) {
-	const $idPokemonElegido = document.querySelector(".id-pokemon-elegido");
-	$idPokemonElegido.textContent = `# ${dataPokemon.id}`;
-}
-
-function mostrarNombrePokemonElegido(dataPokemon) {
-	const $nombrePokemonElegido = document.querySelector(".nombre-pokemon-elegido");
-	$nombrePokemonElegido.textContent = dataPokemon.name;
-}
-
-function mostrarTiposPokemonElegido(dataPokemon) {
-	const cantidadTiposPokemon = dataPokemon.types.length;
-
-	if (cantidadTiposPokemon === 1) {
-		const $primerImagenTipoPokemon = document.querySelector(".primer-tipo-pokemon-elegido");
-		$primerImagenTipoPokemon.src = `img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
-
-		const $segundaImagenTipoPokemon = document.querySelector(".segundo-tipo-pokemon-elegido");
-		$segundaImagenTipoPokemon.id = "oculto";
-	} else {
-		const $primerImagenTipoPokemon = document.querySelector(".primer-tipo-pokemon-elegido");
-		const $segundaImagenTipoPokemon = document.querySelector(".segundo-tipo-pokemon-elegido");
-
-		$primerImagenTipoPokemon.src = `img/${dataPokemon.types["0"]["type"]["name"]}.svg`;
-		$segundaImagenTipoPokemon.src = `img/${dataPokemon.types["1"]["type"]["name"]}.svg`;
-		$segundaImagenTipoPokemon.id = "";
-	}
-}
-
-function mostrarStatsPokemon(dataPokemon) {
-	let vidaPokemon = dataPokemon.stats["0"]["base_stat"];
-	let ataquePokemon = dataPokemon.stats["1"]["base_stat"];
-	let defensaPokemon = dataPokemon.stats["2"]["base_stat"];
-	let ataqueEspecialPokemon = dataPokemon.stats["3"]["base_stat"];
-	let defensaEspecialPokemon = dataPokemon.stats["4"]["base_stat"];
-	let velocidadPokemon = dataPokemon.stats["5"]["base_stat"];
-
-	const $respuestaVidaPokemon = document.querySelector(".vida-base-respuesta");
-	const $respuestaAtaquePokemon = document.querySelector(".ataque-base-respuesta");
-	const $respuestaDefensaPokemon = document.querySelector(".defensa-base-respuesta");
-	const $respuestaAtaqueEspecialPokemon = document.querySelector(".ataque-especial-base-respuesta");
-	const $respuestaDefensaEspecialPokemon = document.querySelector(".defensa-especial-base-respuesta");
-	const $respuestaVelocidadPokemon = document.querySelector(".velocidad-base-respuesta");
-
-	$respuestaVidaPokemon.textContent = vidaPokemon;
-	$respuestaAtaquePokemon.textContent = ataquePokemon;
-	$respuestaDefensaPokemon.textContent = defensaPokemon;
-	$respuestaAtaqueEspecialPokemon.textContent = ataqueEspecialPokemon;
-	$respuestaDefensaEspecialPokemon.textContent = defensaEspecialPokemon;
-	$respuestaVelocidadPokemon.textContent = velocidadPokemon;
 }
 
 function esconderGrilla() {
