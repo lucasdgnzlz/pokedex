e53c4fae7c114808f076ea2f111b5f6b6d1caff3
Lucas <lucasdgonzalez@outlook.com>
2023-11-09 17:17:29 -0300

    Modifica configuración de Jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   setupFiles: ["jest-localstorage-mock"],
-  testEnvironment: 'node, jsdom',
+  testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  testPathIgnorePatterns: ['/node_modules/', '.*.fixture.js'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
