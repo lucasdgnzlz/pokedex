6253c8d346bb8468b83e94916d5f197bafe94c7c
Lucas <lucasdgonzalez@outlook.com>
2023-11-09 17:42:39 -0300

    Prueba la función 'mostrarImagenPokemon'
    Se pone a prueba la función 'mostrarImagenPokemon' pasándole la data cargada de localStorage.

--- src/ui/__tests__/cartasPokemon.spec.js
+++ src/ui/__tests__/cartasPokemon.spec.js
@@ -19,4 +19,19 @@ describe(("mostrarImagenPokemon"), () =>{
 		mostrarImagenPokemon(fixturePokemon, INDICADOR_DE_PRUEBA);
 		expect(document.querySelectorAll(".imagen-carta")[INDICADOR_DE_PRUEBA].src).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png");
 	});
+
+	it(("Muestra la imagen del pokémon con la data cargada del localStorage"), () =>{
+		localStorage.clear();
+		
+		document.body.innerHTML = fixtureCartasPokemon;
+		const INDICADOR_DE_PRUEBA = 3;
+		
+		const dataPokemonAGuardar = fixturePokemonDePrueba;
+		guardarDataPokemonEnLocalStorage(dataPokemonAGuardar);
+
+		const dataPokemon = cargarDataPokemonDeLocalStorage(dataPokemonAGuardar["id"]);
+
+		mostrarImagenPokemon(dataPokemon, INDICADOR_DE_PRUEBA);
+		expect(document.querySelectorAll(".imagen-carta")[INDICADOR_DE_PRUEBA].src).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png");
+	});
 });
