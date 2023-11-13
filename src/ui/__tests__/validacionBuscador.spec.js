10ca94c29a758ae2be8f60919a1b0125209f22f9
Lucas <lucasdgonzalez@outlook.com>
2023-11-13 17:48:28 -0300

    Agrega test de la función 'eliminarErrorValidacion'

--- src/ui/__tests__/validacionBuscador.spec.js
+++ src/ui/__tests__/validacionBuscador.spec.js
@@ -5,11 +5,11 @@
 /// <reference types="Jest" />
 
 import { mostrarErrorValidacionBuscador, eliminarErrorValidacion } from "../validacionBuscador.js";
-import fixtureHTML from "../../../cypress/fixtures/buscadorPokemon.fixture.js";
+import fixtureBuscador from "../../../cypress/fixtures/buscadorPokemon.fixture.js";
 
 describe(("mostrarErrorValidacionBuscador"), () => {
 	it(("Debería mostrar el error al ingresar un valor distinto a un número"), () => {
-		document.body.innerHTML = fixtureHTML;
+		document.body.innerHTML = fixtureBuscador;
 		const mensajeError = "error";
 
 		expect(document.querySelector(".buscador-pokemon").id).toEqual("");
@@ -18,5 +18,24 @@ describe(("mostrarErrorValidacionBuscador"), () => {
 
 		expect(document.querySelector(".buscador-pokemon").placeholder).toEqual(mensajeError);
 		expect(document.querySelector(".buscador-pokemon").id).toEqual("error-validacion");
+	});
+});
+
+describe(("eliminarErrorValidacion"), () => {
+	it(("Debería eliminar los errores mostrados al usuario"), () => {
+		document.body.innerHTML = fixtureBuscador;
+		const mensajeError = "error";
+
+		expect(document.querySelector(".buscador-pokemon").id).toEqual("");
+
+		mostrarErrorValidacionBuscador(mensajeError);
+
+		expect(document.querySelector(".buscador-pokemon").placeholder).toEqual(mensajeError);
+		expect(document.querySelector(".buscador-pokemon").id).toEqual("error-validacion");
+
+		eliminarErrorValidacion();
+
+		expect(document.querySelector(".buscador-pokemon").placeholder).toEqual("Seleccione un Pokémon!");
+		expect(document.querySelector(".buscador-pokemon").id).toEqual("");
 	});
 });
