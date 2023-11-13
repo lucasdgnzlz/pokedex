5d0369d0e7cf1397d2cac54cf4b82aa7711c33ab
Lucas <lucasdgonzalez@outlook.com>
2023-11-13 17:39:22 -0300

    Agrega fixture html del buscadorPokemon

---
+++ cypress/fixtures/buscadorPokemon.fixture.js
@@ -0,0 +1,12 @@
+export default `<div class="contenedor-buscador">
+<div class="input-group">
+  <input
+    type="number"
+    class="form-control buscador-pokemon"
+    aria-label="buscar-pokemon"
+    placeholder="Seleccione un Pokémon!"
+    id=""
+  />
+  <button type="button" class="btn btn-outline-secondary boton-buscar-pokemon">Buscar</button>
+</div>
+</div>`;
