0ea2674a8145db1b40596df65df481f95dbfcb7a
Lucas <lucasdgonzalez@outlook.com>
2023-11-13 18:01:17 -0300

    Agrega test de la función 'esconderPaginador'

--- src/ui/__tests__/paginador.spec.js
+++ src/ui/__tests__/paginador.spec.js
@@ -14,3 +14,18 @@ import {
 	desactivarBotonAnteriorPagina, 
 	activarBotonSiguientePagina, 
 	desactivarBotonSiguientePagina } from "../paginador";
+
+import paginadorFixture from "../../../cypress/fixtures/paginador.fixture.js";
+
+describe(("esconderPaginador"), () => {
+	it(("Debería esconder el paginador"), () => {
+		document.body.innerHTML = paginadorFixture;
+		const $paginador = document.querySelector(".contenedor-cambio-pagina");
+
+		expect($paginador.id).toEqual("");
+
+		esconderPaginador();
+
+		expect($paginador.id).toEqual("oculto");
+	});
+});
