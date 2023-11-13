ebe60225e4e2709665ad35d126ff95d208f6a429
Lucas <lucasdgonzalez@outlook.com>
2023-11-13 19:51:31 -0300

    Agrega test de 'actualizarNumerosIndicadorPagina'
    Este test actualiza los números del paginador como si se le hubiera dado click al botón 'anterior', imprimiendo 3 números menores a los respectivos números anteriormente puestos en su posición.

--- src/ui/__tests__/paginador.spec.js
+++ src/ui/__tests__/paginador.spec.js
@@ -44,3 +44,31 @@ describe(("mostrarPaginador"), () => {
 		expect($paginador.id).toEqual("");
 	});
 });
+
+describe(("actualizarNumerosIndicadorPagina"), () => {
+	it(("Actualiza el paginador imprimiendo un número previo al de la página actual"), () => {
+		document.body.innerHTML = paginadorFixture;
+		const $indicadoresPagina = document.querySelectorAll(".indicador-pagina");
+		const paginaActual = Number(document.querySelector(".active").textContent);
+
+		const numerosEnPaginador = [];
+		const numerosAObtener = [];
+		const accionarDePrueba = "anterior";
+		const paginaSolicitada = paginaActual - 1;
+
+		$indicadoresPagina.forEach(($pagina) => {
+			numerosEnPaginador.push(Number($pagina.textContent));
+			numerosAObtener.push(Number($pagina.textContent) - 1);
+		});
+
+		$indicadoresPagina.forEach(($pagina, i) => {
+			expect(Number($pagina.textContent)).toEqual(numerosEnPaginador[i]);
+		});
+
+		actualizarNumerosIndicadorPagina(accionarDePrueba, $indicadoresPagina, paginaSolicitada);
+
+		$indicadoresPagina.forEach(($pagina, i) => {
+			expect(Number($pagina.textContent)).toEqual(numerosAObtener[i]);
+		});
+	});
+});
