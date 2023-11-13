94c2f5af2b3b22e31816464e7fba2f6cf1750309
Lucas <lucasdgonzalez@outlook.com>
2023-11-13 17:56:14 -0300

    Importa las funciones a testear

---
+++ src/ui/__tests__/paginador.spec.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/// <reference types="Jest" />
+
+import {
+	esconderPaginador, 
+	mostrarPaginador, 
+	actualizarNumerosIndicadorPagina, 
+	desactivarPaginaActiva, 
+	mostrarNumeroPaginaActiva, 
+	activarBotonAnteriorPagina, 
+	desactivarBotonAnteriorPagina, 
+	activarBotonSiguientePagina, 
+	desactivarBotonSiguientePagina } from "../paginador";
