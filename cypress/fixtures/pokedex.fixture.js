d94917c45ef6e6bf9e80884f7e8ee85dfaa4aaeb
Lucas <lucasdgonzalez@outlook.com>
2023-11-15 21:32:28 -0300

    Agrega fixture de la página principal

---
+++ cypress/fixtures/pokedex.fixture.js
@@ -0,0 +1,517 @@
+export default `<div class="row contenedor-estructura-main" id="oculto">
+<div class="col pattern-diagonal-stripes-lg columna-decoracion"></div>
+
+<div class="col-10 columna-contenido">
+  <div class="container contenedor-grilla text-center">
+    <div class="row">
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col columna-grilla">
+        <div class="carta">
+          <div class="contenedor-imagen-pokemon">
+            <img src="" class="imagen-carta" alt="" />
+          </div>
+          <div class="contenedor-info-pokemon">
+            <div class="contenedor-id-pokemon">
+              <p class="numero-identificacion-pokemon"></p>
+            </div>
+            <div class="contenedor-nombre-pokemon">
+              <h5 class="nombre-pokemon">...</h5>
+            </div>
+          </div>
+
+          <div class="contenedor-tipos-pokemon">
+            <div class="contenedor-tipo">
+              <img src="" class="primer-tipo imagen-tipos" />
+            </div>
+            <div class="contenedor-tipo">
+              <img src="" class="segundo-tipo imagen-tipos" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="contenedor-cambio-pagina">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination paginador">
+        <li class="page-item indicador-estado-anterior disabled">
+          <a class="page-link boton-anterior-pagina" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item pagina-item active"><a class="page-link indicador-pagina">1</a></li>
+        <li class="page-item pagina-item"><a class="page-link indicador-pagina">2</a></li>
+        <li class="page-item pagina-item"><a class="page-link indicador-pagina">3</a></li>
+        <li class="page-item indicador-estado-siguiente">
+          <a class="page-link boton-siguiente-pagina" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+
+<div class="col pattern-diagonal-stripes-lg columna-decoracion"></div>
+</div>
+
+<div class="pantalla-de-carga" id="">
+  <p>Cargando...</p>
+</div>`;
