beb9865110d83e6a4646b3edc2f57cebcc735e24
Lucas <lucasdgonzalez@outlook.com>
2023-11-17 19:47:40 -0300

    Modifica nuevamente la función

--- src/api/pokedex.js
+++ src/api/pokedex.js
@@ -2,9 +2,13 @@ export const URL_BASE = "https://pokeapi.co/api/v2/pokemon";
 export const LIMITE_POKEMONES = 20;
 
 export async function hacerSolicitud(indicadorPokemon) {
-	const respuesta = await fetch(`${URL_BASE}?offset=${indicadorPokemon}&limit=${LIMITE_POKEMONES}`);
-	const data = await respuesta.json();
-	return data;
+	try {
+		const respuesta = await fetch(`${URL_BASE}?offset=${indicadorPokemon}&limit=${LIMITE_POKEMONES}`);
+		const data = await respuesta.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 export async function buscarPokemonEspecifico(idPokemon) {
