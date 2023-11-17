47cf693d193b5d428c2a3781dedafa84adf3fe83
Lucas <lucasdgonzalez@outlook.com>
2023-11-17 19:32:50 -0300

    Modifica función 'hacerSolicitud'
    La función tenía un bloque de try catch innecesario

--- src/api/pokedex.js
+++ src/api/pokedex.js
@@ -2,13 +2,9 @@ export const URL_BASE = "https://pokeapi.co/api/v2/pokemon";
 export const LIMITE_POKEMONES = 20;
 
 export async function hacerSolicitud(indicadorPokemon) {
-	try {
-		const respuesta = await fetch(`${URL_BASE}?offset=${indicadorPokemon}&limit=${LIMITE_POKEMONES}`);
-		const data = await respuesta.json();
-		return data;
-	} catch (error) {
-		console.error(error);
-	}
+	const respuesta = await fetch(`${URL_BASE}?offset=${indicadorPokemon}&limit=${LIMITE_POKEMONES}`);
+	const data = await respuesta.json();
+	return data;
 }
 
 export async function buscarPokemonEspecifico(idPokemon) {
